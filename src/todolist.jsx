5ab510163231138393fa576004a34dced4b2f7f5
maicolJubriny <89138553+GuatonMordekaizer@users.noreply.github.com>
2025-07-24 19:21:38 -0400

    cambios 1

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -23,11 +23,12 @@ export function TodoList(){
 
 export function TodoList(){
     const [todos,setTodos] = useState([
+     // todos es un arreglo (estado)  y setTodos es el metodo que me permitira cambiar el estado   
       //  {id:1}, {id:2},{id:3},{id:4}
-        {id:1, task:'Tarea 1'},
-        {id:2, task:'Tarea 2'},
-        {id:3, task:'Tarea 3'},
-        {id:4, task:'Tarea 4'}
+        {id:1, task:'Tarea 1', completed:true},
+        {id:2, task:'Tarea 2', completed:false},
+        {id:3, task:'Tarea 3', completed:true},
+        {id:4, task:'Tarea 4', completed:false}
     ]);
 
     const taskRef = useRef();
@@ -41,14 +42,62 @@ export function TodoList(){
         setTodos((prevTodos) => {
             const newTask = {
                 id:uuid(),   // para instalar esta función  npm install uuid  y se debe importar 
-                task: task
+                task: task,
+                completed:false   //  para registrar si la tarea si se completo la tarea o se encuentra pendiente.
+
             }
             return [...prevTodos,newTask]
         })
 
     }
 
-    // todos es un arreglo (estado)  y setTodos es el metodo que me permitira cambiar el estado   
+    const ResumenTareas = () =>{    //  COMPONENTE LOCAL Resumen Tarea
+        const cant = cantidadTareas()
+        if (cant == 0) {
+            return(
+                <div className="alert alert-success mt-3">
+                    <h2>RESUMEN TAREAS </h2>
+                    <h3>Felicitaciones no tienes tareas pendientes </h3>
+                </div>
+                )
+
+        }
+        
+        if (cant == 1) {
+            return(
+                <div className="alert alert-info mt-3">
+                    <h2>RESUMEN TAREAS </h2>
+                    <h3>Queda solamente una tarea pendiente : {cant}</h3>
+                </div>
+                )
+
+        }
+      return(
+                <div className="alert alert-info mt-3">
+                    <h2>RESUMEN TAREAS </h2>
+                    <h3>Tareas pendientes : {cant}</h3>
+      </div>)
+
+    }
+
+    const cantidadTareas = () =>{
+        return todos.filter((todo)=> !todo.completed).length;
+    }
+
+    const cambiarEstadoTarea = (id) => {
+        console.log(id)      
+         const newTodos = [...todos];  //   Hace una copia de elemento     
+         const todo = newTodos.find((todo)=> todo.id === id)  
+         todo.completed = !todo.completed;
+         setTodos(newTodos)   
+    }
+
+    const eliminarTareasCompletadas=()=> {
+        const newTodos = todos.filter((todo) => !todo.completed);
+        setTodos(newTodos);
+    }
+
+   
 
     return(
 
@@ -57,13 +106,16 @@ export function TodoList(){
             <div className="input-group mt-4 mb-4">
                 <input className="form-control" ref={taskRef} placeholder="Agregar Tarea" type="text"></input>
                 <button onClick={agregarTarea} className="btn btn-success ms-2" >+</button>
+                <button onClick={eliminarTareasCompletadas} className="btn btn-danger ms-2" >-</button>
             </div>
 
             <ul className="list-group">
                 {todos.map((todo) =>(
-                    <TodoItem todo={todo} key={todo.id}></TodoItem>
+                    <TodoItem todo={todo} key={todo.id} cambiarEstado={cambiarEstadoTarea} ></TodoItem>
                 ))}
             </ul>
+
+             <ResumenTareas/> {/* Nuevo Compontente  "Local" */}
          </Fragment>
     );
 }
